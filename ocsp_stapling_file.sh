e73c8010be5994e3759a8a37907978341b80bfb8
practicemp <practicemphw@163.com>
2019-01-17 22:52:24 +0800

    补充代码及注释

--- ocsp_stapling_file.sh
+++ ocsp_stapling_file.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # author: practicemp
+# url: https://github.com/practicemp/ocsp
+# 获取 Let's Encrypt 的 OCSP 响应文件，应用于 Nginx 配置文件中的 ssl_stapling_file 指令。
+# 用法请查看上面的项目主页。
 
 # 如果传入的字符串 $1 为空，则发出自定义信息 $2 并退出脚本。
 emptyString(){
@@ -8,10 +11,10 @@ emptyString(){
 
 # 如果传入的文件不存在，则发出信息，并退出脚本。
 nonexistentFile(){
-    if [ ! -e $1 || ! -r $1 ]; then echo $1 "不存在或不可读"; exit 1; fi
+    if [[ ! -e $1 || ! -r $1 ]]; then echo $1 "不存在或不可读"; exit 1; fi
 }
 
-emptyString "$1" "域名参数未输入"
+emptyString "$1" "证书域名参数未输入"
 domain=$1
 
 emptyString "$2" "OCSP 响应文件路径参数未输入"
@@ -45,9 +48,11 @@ version_info=`openssl version`
 version=${version_info:0:13}
 
 case $version in
+    'OpenssL 1.0.2') header="Host ${ocsp_url_host}"
+        ;;
     'OpenSSL 1.1.0'|'OpenSSL 1.1.1') header="Host=${ocsp_url_host}"
         ;;
-    *) header="Host ${ocsp_url_host}"
+    *) echo $version" 该版本尚未测试，所以不支持，详情查看项目主页：https://github.com/practicemp/ocsp"; exit
         ;;
 esac
 
@@ -60,10 +65,13 @@ do
         diff $ocsp_resp_temp_file $ocsp_resp_file >/dev/null 2>&1
         if [ $? != 0 ]; then
             cat $ocsp_resp_temp_file > $ocsp_resp_file
-#            echo "OSCP 响应文件已更新"
+#            rm -f $ocsp_resp_temp_file
+#            echo "响应文件已更新"
+#            echo "$ocsp_resp"
             exit
         else 
 #            echo "新响应内容无变化"
+#            echo "$ocsp_resp"
             exit
         fi
     fi
